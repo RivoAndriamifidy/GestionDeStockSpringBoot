9afcfb835454fe76739cc4ff11a7b94da2b924e1
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-05-24 13:30:08 +0300

    push

--- src/main/java/com/rivo/gestiondestock/model/Ventes.java
+++ src/main/java/com/rivo/gestiondestock/model/Ventes.java
@@ -4,13 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "ventes")
-public class Ventes {
+public class Ventes extends AbstractEntity{
 
 }
