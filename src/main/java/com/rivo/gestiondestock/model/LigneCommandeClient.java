d0db6af1194642d020cdaa6b3bb1e6b0714ec436
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-05-31 11:39:14 +0300

    creation d'entitée

--- src/main/java/com/rivo/gestiondestock/model/LigneCommandeClient.java
+++ src/main/java/com/rivo/gestiondestock/model/LigneCommandeClient.java
@@ -1,6 +1,10 @@
 package com.rivo.gestiondestock.model;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,5 +18,13 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "lignecommandeclient")
 public class LigneCommandeClient extends AbstractEntity{
+	
+	@ManyToOne
+	@JoinColumn(name = "idarticle")
+	private Article artcile;
+	
+	@ManyToOne
+	@JoinColumn(name = "idcommandeclient")
+	private CommandeClient commendeClient;
 
 }
