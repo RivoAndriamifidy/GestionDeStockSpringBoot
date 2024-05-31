86e3cfdee1ca612e713a3c950c7f8b7f9654f776
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-05-31 12:01:49 +0300

    creation d'entitée

--- src/main/java/com/rivo/gestiondestock/model/CommandeFournisseur.java
+++ src/main/java/com/rivo/gestiondestock/model/CommandeFournisseur.java
@@ -1,6 +1,13 @@
 package com.rivo.gestiondestock.model;
 
+import java.time.Instant;
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,4 +22,16 @@ import lombok.NoArgsConstructor;
 @Table(name = "commandefournisseur")
 public class CommandeFournisseur extends AbstractEntity{
 
+	@Column(name = "code")
+	private String code;
+	
+	@Column(name = "datecommande")
+	private Instant dateCommande;
+	
+	@ManyToOne
+	@JoinColumn(name = "idFournisseur")
+	private Fournisseur fournisseur;
+	
+	@OneToMany(mappedBy = "commandeFournisseur")
+	private List<LingeCommandeFournisseur> ligneCommandeFournisseurs;
 }
