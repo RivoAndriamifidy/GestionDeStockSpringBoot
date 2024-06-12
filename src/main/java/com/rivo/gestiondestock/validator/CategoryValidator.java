f3314e3e6719bd2597ba678bb950e28efce6f63d
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-06-12 16:09:03 +0300

    validator

---
+++ src/main/java/com/rivo/gestiondestock/validator/CategoryValidator.java
@@ -0,0 +1,14 @@
+package com.rivo.gestiondestock.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rivo.gestiondestock.dto.CategoryDto;
+
+public class CategoryValidator {
+	
+	public static List<String> validate(CategoryDto categoryDto){
+		List<String> errors = new ArrayList<>();
+	}
+
+}
