de203ba08227ce1d0e24ba6d7043d6ebb848f891
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-06-13 11:44:33 +0300

    categoryValidator

--- src/main/java/com/rivo/gestiondestock/validator/CategoryValidator.java
+++ src/main/java/com/rivo/gestiondestock/validator/CategoryValidator.java
@@ -3,12 +3,19 @@ package com.rivo.gestiondestock.validator;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.util.StringUtils;
+
 import com.rivo.gestiondestock.dto.CategoryDto;
 
 public class CategoryValidator {
 	
 	public static List<String> validate(CategoryDto categoryDto){
 		List<String> errors = new ArrayList<>();
+		
+		if(categoryDto == null || !StringUtils.hasLength(categoryDto.getCode())) {
+			errors.add("Veuillez renseigner le code de la categorie");
+		}
+		return errors;
 	}
 
 }
