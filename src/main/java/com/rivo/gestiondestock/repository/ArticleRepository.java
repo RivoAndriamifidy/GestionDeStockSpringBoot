941c90ebf035a9f6bbacbb824daab40e6082195d
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-06-19 14:25:13 +0300

    article serviceimpl et article repository

--- src/main/java/com/rivo/gestiondestock/repository/ArticleRepository.java
+++ src/main/java/com/rivo/gestiondestock/repository/ArticleRepository.java
@@ -1,6 +1,8 @@
 package com.rivo.gestiondestock.repository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,15 +10,14 @@ import org.springframework.data.repository.query.Param;
 
 import com.rivo.gestiondestock.model.Article;
 
-public interface ArticleRepository extends JpaRepository<Integer, Article>{
-	
-	@Query("select a from article where codearticl = :code and designation = :designation")
-	List<Article> findByCustomQuery(@Param("code")String c, @Param("designation")String d);
-	
-	@Query(value = "select * from article where code = :code", nativeQuery = true)
-	List<Article> findByCustomNativeQuery(@Param("code") String c);
-	
-	List<Article> findByCodeArticleIngoreCaseAndDesignationIgnoreCase(String codeArticle, String designation);
+public interface ArticleRepository extends JpaRepository<Article, Integer>{
+
+	Optional<Article> findArticleByCodeArticle(String codeArticle);
+
+	static List<Article> findByCategoryId(Integer idCategory) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
--- src/main/java/com/rivo/gestiondestock/services/impl/ArticleServiceImpl.java
+++ src/main/java/com/rivo/gestiondestock/services/impl/ArticleServiceImpl.java
@@ -1,45 +1,152 @@
 package com.rivo.gestiondestock.services.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.rivo.gestiondestock.dto.ArticleDto;
+import com.rivo.gestiondestock.dto.LigneCommandeClientDto;
+import com.rivo.gestiondestock.dto.LigneCommandeFournisseurDto;
+import com.rivo.gestiondestock.dto.LigneVenteDto;
+import com.rivo.gestiondestock.exception.ErrorCodes;
+import com.rivo.gestiondestock.exception.InvalidEntityException;
+import com.rivo.gestiondestock.model.LigneCommandeClient;
+import com.rivo.gestiondestock.model.LigneCommandeFournisseur;
+import com.rivo.gestiondestock.model.LigneVente;
+import com.rivo.gestiondestock.repository.ArticleRepository;
+import com.rivo.gestiondestock.repository.LigneCommandeClientRepository;
+import com.rivo.gestiondestock.repository.LigneCommandeFournisseurRepository;
+import com.rivo.gestiondestock.repository.LigneVenteRepository;
 import com.rivo.gestiondestock.services.ArticleService;
+import com.rivo.gestiondestock.validator.ArticleValidator;
+
+import com.rivo.gestiondestock.exception.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class ArticleServiceImpl implements ArticleService{
 
+	private ArticleRepository articleRepository;
+	private LigneVenteRepository venteRepository;
+	private LigneCommandeFournisseurRepository commandeFournisseurRepository;
+	private LigneCommandeClientRepository commandeClientRepository;
+	
+	public ArticleServiceImpl(
+			ArticleRepository articleRepository,
+			LigneVenteRepository venteRepository,
+			LigneCommandeFournisseurRepository commandeFournisseurRepository,
+			LigneCommandeClientRepository commandeClientRepository){
+		this.articleRepository = articleRepository;
+		this.venteRepository = venteRepository;
+		this.commandeFournisseurRepository = commandeFournisseurRepository;
+		this.commandeClientRepository = commandeClientRepository;		
+	}
+	
 	@Override
 	public ArticleDto save(ArticleDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> errors = ArticleValidator.validate(dto);
+		if(!errors.isEmpty()) {
+			log.error("Article is not valid {}", dto);
+			throw new InvalidEntityException("L'article n'est pas valide", ErrorCodes.ARTICLE_NOT_VALID, errors);
+		}
+		return ArticleDto.fromEntity(articleRepository.save(ArticleDto.toEntity(dto)));
 	}
 
 	@Override
 	public ArticleDto findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		if(id == null) {
+			log.error("Article ID is null");
+			return null;
+		}
+		return articleRepository.findById(id)
+				.map(ArticleDto::fromEntity)
+				.orElseThrow(() -> new EntityNotFoundException("Aucun Article avec l'ID = " + id + "n'a été trouvé dans la BDD",
+						ErrorCodes.ARTICLE_NOT_FOUND) 
+			);
 	}
 
 	@Override
 	public ArticleDto findByCodeArticle(String codeArticle) {
-		// TODO Auto-generated method stub
-		return null;
+		if(!StringUtils.hasLength(codeArticle)) {
+			log.error("Article CODE is null");
+			return null;
+		}
+		return articleRepository.findArticleByCodeArticle(codeArticle)
+				.map(ArticleDto::fromEntity)
+				.orElseThrow(() -> new EntityNotFoundException("Aucun Article avec le CODE = " + codeArticle + "n'a été trouvé dans la BDD",
+						ErrorCodes.ARTICLE_NOT_FOUND) 
+			);
 	}
 
 	@Override
 	public List<ArticleDto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return articleRepository.findAll().stream()
+				.map(ArticleDto::fromEntity)
+				.collect(Collectors.toList());
+	}
+
+
+	@Override
+	public List<LigneVenteDto> findHistoriqueVentes(Integer idArticle) {
+	
+		return venteRepository.findAllByArticleId(idArticle).stream()
+				.map(LigneVenteDto::fromEntity)
+				.collect(Collectors.toList());
 	}
 
 	@Override
-	public void delete(Integer id) {
-		// TODO Auto-generated method stub
+	public List<LigneCommandeClientDto> findHistoriqueCommandeClient(Integer idArticle) {
 		
+		return commandeClientRepository.findAllByArticleId(idArticle).stream()
+				.map(LigneCommandeClientDto::fromEntity)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public List<LigneCommandeFournisseurDto> fingHistoriqueCommandeFournisseur(Integer idArtcile) {
+		
+		return commandeFournisseurRepository.findAllByArticleId(idArtcile).stream()
+				.map(LigneCommandeFournisseurDto::fromEntity)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public List<ArticleDto> findAllArticleByIdCategory(Integer idCategory) {
+		
+		return ArticleRepository.findByCategoryId(idCategory).stream()
+				.map(ArticleDto::fromEntity)
+				.collect(Collectors.toList());
 	}
 	
-	
+	@Override
+	public void delete(Integer id) {
+		
+		if(id == null) {
+			log.error("Article ID is null");
+			return;
+		}
+		
+		List<LigneCommandeClient> ligneCommandeClients = commandeClientRepository.findAllByArticleId(id);
+		if(!ligneCommandeClients.isEmpty()) {
+			throw new InvalidEntityException("Impossible de supprimer un article deja utilisé dans des commandes client", ErrorCodes.ARTICLE_ALREADY_IN_USE);
+		}
+		
+		List<LigneCommandeFournisseur> ligneCommandeFournisseurs = commandeFournisseurRepository.findAllByArticleId(id);
+		if(!ligneCommandeFournisseurs.isEmpty()) {
+			throw new InvalidEntityException("Impossible de supprimer un article deja utilisé dans des commandes fournisseur", ErrorCodes.ARTICLE_ALREADY_IN_USE);
+		}
+		
+		List<LigneVente> ligneVentes = venteRepository.findAllByArticleId(id);
+		if(!ligneVentes.isEmpty()) {
+			throw new InvalidEntityException("Impossible de supprimer un article deja utilise dans des ventes", ErrorCodes.ARTICLE_ALREADY_IN_USE);
+		}
+		
+		articleRepository.deleteById(id);
+		
+	}
 
 }
